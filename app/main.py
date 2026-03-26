e3319775f049bffb3d22bd5ebbe36d4f98fd82d2
Alexiz Hernandez <alexiz.developer@gmail.com>
2026-03-26 03:27:51 -0700

    updated cors

--- app/main.py
+++ app/main.py
@@ -42,7 +42,7 @@ app.add_middleware(get_middleware())
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=[settings.FRONTEND_URL],
     allow_credentials=True,
     allow_methods=["GET", "PUT", "POST", "DELETE", "OPTIONS", "PATCH"],
     allow_headers=["Content-Type"] + get_all_cors_headers(),
